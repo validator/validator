6d42971f1e08cbc9e253aaa6551ec590d59074f3
James Clark <jjc@jclark.com>
2001-06-09 03:40:31 +0000

    Add build support

---
+++ jing-trang/scripts/build-jing.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+SET JDK=c:\jdk1.3
+set JING_HOME=%~d0%~p0..
+%JDK%\bin\java -Dant.home=%JING_HOME% -cp %JING_HOME%\lib\ant.jar;%JING_HOME%\lib\saxon.jar;%JING_HOME%\lib\jaxp.jar;%JING_HOME%\lib\crimson.jar;%JING_HOME%\lib\optional.jar;%JDK%\lib\tools.jar;%JING_HOME%\lib\junit.jar;%JING_HOME%\lib\regex.jar org.apache.tools.ant.Main -buildfile %JING_HOME%\build.xml %*
