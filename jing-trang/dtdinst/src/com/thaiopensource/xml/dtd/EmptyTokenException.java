a1c476c6f7b8fd0d2a6a5adf27059ec615739b51
James Clark <jjc@jclark.com>
2001-08-14 06:48:14 +0000

    Adapted from XP

---
+++ jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/EmptyTokenException.java
@@ -0,0 +1,7 @@
+package com.thaiopensource.xml.dtd;
+
+/**
+ * Thrown to indicate that the subarray being tokenized is empty.
+ */
+public class EmptyTokenException extends TokenException {
+}
---
+++ jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/TokenException.java
@@ -0,0 +1,10 @@
+package com.thaiopensource.xml.dtd;
+
+/**
+ * The superclass of all checked exceptions thrown by methods in
+ * <code>Tokenizer</code>.
+ *
+ * @see Tokenizer
+ */
+public class TokenException extends Exception {
+}
