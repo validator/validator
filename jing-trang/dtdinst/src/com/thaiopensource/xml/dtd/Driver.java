21d5862b3d6ebbe49c46c55b0c34f3936195c528
James Clark <jjc@jclark.com>
2001-08-19 03:59:56 +0000

    Flush writer.

--- jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/Driver.java
+++ jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/Driver.java
@@ -6,6 +6,8 @@ import java.io.OutputStreamWriter;
 
 public class Driver {
   public static void main (String args[]) throws IOException {
-    new SchemaWriter(new XmlWriter(new BufferedWriter(new OutputStreamWriter(System.out)))).writeDtd(new Dtd(args[0]));
+    BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+    new SchemaWriter(new XmlWriter(w)).writeDtd(new Dtd(args[0]));
+    w.flush();
   }
 }
