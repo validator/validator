6d37d7ee367f9dcc246697a50735b750ad8567ab
James Clark <jjc@jclark.com>
2001-08-21 06:27:29 +0000

    *** empty log message ***

---
+++ jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/NotationDecl.java
@@ -0,0 +1,28 @@
+package com.thaiopensource.xml.dtd;
+
+public class NotationDecl extends TopLevel {
+  
+  private final String name;
+  private final ExternalId externalId;
+
+  public NotationDecl(String name, ExternalId externalId) { 
+    this.name = name;
+    this.externalId = externalId;
+  }
+
+  public int getType() {
+    return NOTATION_DECL;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public ExternalId getExternalId() {
+    return externalId;
+  }
+
+  public void accept(TopLevelVisitor visitor) throws Exception {
+    visitor.notationDecl(name, externalId);
+  }
+}
