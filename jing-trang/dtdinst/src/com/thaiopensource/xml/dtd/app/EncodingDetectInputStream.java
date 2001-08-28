774f8c57d8d07d3a2efa043c9423d632fc0b26b1
James Clark <jjc@jclark.com>
2001-08-28 14:38:06 +0000

    Support EBCDIC.

--- jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/app/EncodingDetectInputStream.java
+++ jing-trang/dtdinst/src/com/thaiopensource/xml/dtd/app/EncodingDetectInputStream.java
@@ -18,23 +18,25 @@ public class EncodingDetectInputStream extends InputStream {
   }
 
   private static final short[] detectProg = { 
-    // number of bytes, bytes,..., loByteIndex, bytesPerChar, bomLength,
-    4, 0x00, 0x00, 0xFE, 0xFF, 3, 4, 4, 
-    4, 0xFF, 0xFE, 0x00, 0x00, 0, 4, 4,
-    4, 0x00, 0x00, 0xFF, 0xFE, 2, 4, 4,
-    4, 0xFE, 0xFF, 0x00, 0x00, 1, 4, 4,
-    2, 0xFE, 0xFF,             1, 2, 2,
-    2, 0xFF, 0xFE,             0, 2, 2,
-    3, 0xEF, 0xBB, 0xBF,       0, 1, 3,
-    4, 0x00, 0x00, 0x00, 0x3C, 3, 4, 0, 
-    4, 0x3C, 0x00, 0x00, 0x00, 0, 4, 0,
-    4, 0x00, 0x00, 0x3C, 0x00, 2, 4, 0,
-    4, 0x00, 0x3C, 0x00, 0x00, 1, 4, 0,
-    4, 0x00, 0x3C, 0x00, 0x3F, 1, 2, 0,
-    4, 0x3C, 0x00, 0x3F, 0x00, 0, 2, 0, 
-    4, 0x3C, 0x3F, 0x78, 0x6D, 0, 1, 0 
-  //4, 0x4C, 0x6F, 0xA7, 0x94, 0, 1, 0,
+    // num bytes, bytes,..., loByteIndex, bytesPerChar, bomLength, encType,
+    4, 0x00, 0x00, 0xFE, 0xFF, 3, 4, 4, 0,
+    4, 0xFF, 0xFE, 0x00, 0x00, 0, 4, 4, 0,
+    4, 0x00, 0x00, 0xFF, 0xFE, 2, 4, 4, 0,
+    4, 0xFE, 0xFF, 0x00, 0x00, 1, 4, 4, 0,
+    2, 0xFE, 0xFF,             1, 2, 2, 0,
+    2, 0xFF, 0xFE,             0, 2, 2, 0,
+    3, 0xEF, 0xBB, 0xBF,       0, 1, 3, 0,
+    4, 0x00, 0x00, 0x00, 0x3C, 3, 4, 0, 0,
+    4, 0x3C, 0x00, 0x00, 0x00, 0, 4, 0, 0,
+    4, 0x00, 0x00, 0x3C, 0x00, 2, 4, 0, 0,
+    4, 0x00, 0x3C, 0x00, 0x00, 1, 4, 0, 0,
+    4, 0x00, 0x3C, 0x00, 0x3F, 1, 2, 0, 0,
+    4, 0x3C, 0x00, 0x3F, 0x00, 0, 2, 0, 0,
+    4, 0x3C, 0x3F, 0x78, 0x6D, 0, 1, 0, 0,
+    4, 0x4C, 0x6F, 0xA7, 0x94, 0, 1, 0, 1
   };
+
+  static int NPARMS = 4;
 
   /**
    * Detects the encoding based on the bytes following the current
@@ -48,6 +50,7 @@ public class EncodingDetectInputStream extends InputStream {
     int bytesPerChar = 1;
     int loByteIndex = 0;
     int bomLength = 0;
+    int encType = 0;
 
     int pc = 0;
     while (pc < detectProg.length) {
@@ -64,20 +67,24 @@ public class EncodingDetectInputStream extends InputStream {
 	  loByteIndex = detectProg[pc++];
 	  bytesPerChar = detectProg[pc++];
 	  bomLength = detectProg[pc++];
+	  encType = detectProg[pc++];
 	  break;
 	}
       }
-      pc += nBytes + 3;
+      pc += nBytes + NPARMS;
     }
     int chIndex = 0;
     boolean prevCharQuestion = false;
     boolean gotXmlDecl = false;
     
     while (makeAvailable((chIndex + 1)*bytesPerChar + bomLength)) {
-      char ch = (char)buf[start + bomLength +chIndex*bytesPerChar + loByteIndex];
+      byte b = buf[start + bomLength +chIndex*bytesPerChar + loByteIndex];
       for (int i = 0; i < bytesPerChar; i++)
         if (i != loByteIndex && buf[start + bomLength + chIndex*bytesPerChar + i] != 0)
           throw new CharConversionException("non-ASCII character in encoding declaration");
+      char ch = convertByte(b, encType);
+      if (ch >= 0x80)
+	throw new CharConversionException("non-ASCII character in encoding declaration");
       if (chIndex < OPEN.length()) {
 	if (ch != OPEN.charAt(chIndex))
 	  break;
@@ -97,7 +104,8 @@ public class EncodingDetectInputStream extends InputStream {
     if (gotXmlDecl) {
       char[] b = new char[chIndex];
       for (int i = 0; i < chIndex; i++)
-	b[i] = (char)buf[start + bomLength + i*bytesPerChar + loByteIndex];
+	b[i] = convertByte(buf[start + bomLength + i*bytesPerChar + loByteIndex],
+			   encType);
       try {
 	TextDecl decl = new TextDecl(b, 0, b.length);
 	enc = decl.getEncoding();
@@ -112,13 +120,27 @@ public class EncodingDetectInputStream extends InputStream {
       avail -= bomLength;
     }
     if (enc == null) {
-      if ((bytesPerChar == 2 && bomLength == 0) || bytesPerChar > 2)
+      if ((bytesPerChar == 2 && bomLength == 0)
+	  || bytesPerChar > 2
+	  || encType != 0)
 	throw new CharConversionException("missing encoding declaration");
       if (bytesPerChar == 2)
 	return "UTF-16";
       return "UTF-8";
     }
     return enc;
+  }
+
+  static final String EBCDIC_ENCODING = "Cp037";
+
+  static private char convertByte(byte b, int encType) throws IOException {
+    if (encType == 1) {
+      String s = new String(new byte[]{b}, EBCDIC_ENCODING);
+      if (s.length() != 1)
+	throw new CharConversionException();
+      return s.charAt(0);
+    }
+    return (char)(b & 0xFF);
   }
 
   private static final int INIT_BUF_SIZE = 80;
