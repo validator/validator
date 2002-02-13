25e8ced5ddd2f345230eeb527c6a4c66b65e904a
James Clark <jjc@jclark.com>
2002-02-13 03:12:13 +0000

    Add regex2.jar

--- jing-trang/scripts/build-jing.bat
+++ jing-trang/scripts/build-jing.bat
@@ -2,4 +2,4 @@
 setlocal
 SET JDK=c:\jdk1.3
 set JING_HOME=%~d0%~p0..
-%JDK%\bin\java -Dant.home=%JING_HOME% -cp %JING_HOME%\lib\ant.jar;%JING_HOME%\lib\saxon.jar;%JING_HOME%\lib\crimson.jar;%JING_HOME%\lib\optional.jar;%JDK%\lib\tools.jar;%JING_HOME%\lib\junit.jar;%JING_HOME%\lib\regex.jar;%JING_HOME%\lib\isorelax.jar org.apache.tools.ant.Main -buildfile %JING_HOME%\build.xml %*
+%JDK%\bin\java -Dant.home=%JING_HOME% -cp %JING_HOME%\lib\ant.jar;%JING_HOME%\lib\saxon.jar;%JING_HOME%\lib\crimson.jar;%JING_HOME%\lib\optional.jar;%JDK%\lib\tools.jar;%JING_HOME%\lib\junit.jar;%JING_HOME%\lib\regex.jar;%JING_HOME%\lib\regex2.jar;%JING_HOME%\lib\isorelax.jar org.apache.tools.ant.Main -buildfile %JING_HOME%\build.xml %*
