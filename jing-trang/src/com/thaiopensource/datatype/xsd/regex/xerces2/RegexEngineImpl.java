91daeb49be5aa4a96078416fc42c2d3883d08496
James Clark <jjc@jclark.com>
2002-02-13 03:19:52 +0000

    Add Xerces 2 regex impl.

---
+++ jing-trang/src/com/thaiopensource/datatype/xsd/regex/xerces2/RegexEngineImpl.java
@@ -0,0 +1,33 @@
+package com.thaiopensource.datatype.xsd.regex.xerces2;
+
+import com.thaiopensource.datatype.xsd.RegexEngine;
+import com.thaiopensource.datatype.xsd.Regex;
+import com.thaiopensource.datatype.xsd.InvalidRegexException;
+
+import org.apache.xerces.impl.xpath.regex.RegularExpression;
+import org.apache.xerces.impl.xpath.regex.ParseException;
+
+public class RegexEngineImpl implements RegexEngine {
+  public RegexEngineImpl() {
+    // Force a linkage error on instantiation if the Xerces classes
+    // are not available.
+    try {
+      new RegularExpression("", "X");
+    }
+    catch (ParseException e) {
+    }
+  }
+  public Regex compile(String expr) throws InvalidRegexException {
+    try {
+      final RegularExpression re = new RegularExpression(expr, "X");
+      return new Regex() {
+	  public boolean matches(String str) {
+	    return re.matches(str);
+	  }
+	};
+    }
+    catch (ParseException e) {
+      throw new InvalidRegexException(e.getMessage(), e.getLocation());
+    }
+  }
+}
