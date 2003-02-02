1db2b0e21f001190cf5432630c5706f93a46205e
James Clark <jjc@jclark.com>
2003-02-02 02:15:46 +0000

    Get rid of unused import

--- jing-trang/src/com/thaiopensource/relaxng/auto/SchemaReceiver.java
+++ jing-trang/src/com/thaiopensource/relaxng/auto/SchemaReceiver.java
@@ -3,10 +3,6 @@ package com.thaiopensource.relaxng.auto;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
-import java.io.IOException;
-
-import com.thaiopensource.relaxng.auto.SchemaFuture;
-
 public interface SchemaReceiver {
   SchemaFuture installHandlers(XMLReader xr) throws SAXException;
 }
