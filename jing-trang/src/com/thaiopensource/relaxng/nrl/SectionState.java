aa70dd437440d5432a8ee7386cbd46c8e84eb521
James Clark <jjc@jclark.com>
2003-05-22 03:04:47 +0000

    Get rid of unused imports

--- jing-trang/src/com/thaiopensource/relaxng/nrl/SectionState.java
+++ jing-trang/src/com/thaiopensource/relaxng/nrl/SectionState.java
@@ -2,9 +2,6 @@ package com.thaiopensource.relaxng.nrl;
 
 import com.thaiopensource.relaxng.ValidatorHandler;
 import com.thaiopensource.relaxng.Schema;
-import com.thaiopensource.util.Localizer;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 
 public interface SectionState {
