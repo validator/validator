ccc884ff7fc858b5aec92551d4db8d96e3499290
James Clark <jjc@jclark.com>
2003-05-30 10:02:33 +0000

    Remove XXXs

--- jing-trang/src/com/thaiopensource/validate/schematron/SchematronSchemaReaderFactory.java
+++ jing-trang/src/com/thaiopensource/validate/schematron/SchematronSchemaReaderFactory.java
@@ -10,8 +10,6 @@ import javax.xml.transform.TransformerConfigurationException;
 import com.icl.saxon.TransformerFactoryImpl;
 
 public class SchematronSchemaReaderFactory implements SchemaReaderFactory {
-  // XXX need property to set phase
-  // XXX need flag for diagnostics
   public SchemaReader createSchemaReader(String namespaceUri) {
     if (namespaceUri.equals(SchemaReaderImpl.SCHEMATRON_URI)) {
       try {
