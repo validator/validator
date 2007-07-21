4dc7a7d7350611642c5fea5f27fd4dc0adb08cac
hsivonen@c6dcca6c-c432-0410-a79e-3fbb6554aff1 <hsivonen@c6dcca6c-c432-0410-a79e-3fbb6554aff1>
2007-07-21 22:07:28 +0000

    the great validator.nu refactoring landing
    --HG--
    extra : convert_revision : svn%3Ac6dcca6c-c432-0410-a79e-3fbb6554aff1/trunk%4025

---
+++ src/nu/validator/servlet/ParserMode.java
@@ -0,0 +1,11 @@
+package nu.validator.servlet;
+
+public enum ParserMode {
+    HTML_AUTO,
+    HTML401_TRANSITIONAL,
+    HTML401_STRICT,
+    HTML,
+    XML_NO_EXTERNAL_ENTITIES,
+    XML_EXTERNAL_ENTITIES_NO_VALIDATION,
+    AUTO
+}
