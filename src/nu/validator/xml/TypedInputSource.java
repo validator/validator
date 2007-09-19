6b17d1cf288f308922ec6e4b2a41c1849905e559
hsivonen@5bfc576b-c432-0410-be1c-015b6bc4b1a2 <hsivonen@5bfc576b-c432-0410-be1c-015b6bc4b1a2>
2007-09-19 12:45:00 +0000

    add length
    --HG--
    extra : convert_revision : svn%3A5bfc576b-c432-0410-be1c-015b6bc4b1a2/trunk%4012

--- src/nu/validator/xml/TypedInputSource.java
+++ src/nu/validator/xml/TypedInputSource.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2005 Henri Sivonen
+ * Copyright (c) 2007 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 package nu.validator.xml;
 
 import java.io.InputStream;
@@ -12,7 +35,9 @@ import org.xml.sax.InputSource;
 public class TypedInputSource extends InputSource {
 
     private String type;
-    
+
+    private int length = -1;
+
     /**
      * 
      */
@@ -53,5 +78,26 @@ public class TypedInputSource extends InputSource {
      */
     public void setType(String type) {
         this.type = type;
+    }
+    
+    /**
+     * Returns the length.
+     * 
+     * @return the length
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * Sets the length.
+     * 
+     * @param length the length to set
+     */
+    public void setLength(int length) {
+        if (length < -1) {
+            throw new IllegalArgumentException("Length must be -1 or greater.");
+        }
+        this.length = length;
     }
 }
