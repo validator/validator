85a273ec9dc01bfa2ce945d33c06c2158c58df70
hsivonen@c6dcca6c-c432-0410-a79e-3fbb6554aff1 <hsivonen@c6dcca6c-c432-0410-a79e-3fbb6554aff1>
2007-09-26 20:26:10 +0000

    JSON emitter
    --HG--
    extra : convert_revision : svn%3Ac6dcca6c-c432-0410-a79e-3fbb6554aff1/trunk%4071

---
+++ src/nu/validator/messages/JsonExtractHandler.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2007 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.messages;
+
+import org.xml.sax.SAXException;
+
+import nu.validator.json.JsonHandler;
+import nu.validator.source.SourceHandler;
+
+public class JsonExtractHandler implements SourceHandler {
+
+    private static final char[] NEWLINE = {'\n'};
+    
+    private enum HiliteState {
+        BEFORE,
+        INSIDE,
+        AFTER
+    }
+    
+    private final JsonHandler handler;
+    
+    private int hiliteStart;
+    
+    private int hiliteLength;
+    
+    private HiliteState hiliteState;
+    
+    public JsonExtractHandler(JsonHandler handler) {
+        this.handler = handler;
+    }
+
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+        handler.characters(ch, start, length);
+        switch (hiliteState) {
+            case BEFORE:
+                hiliteStart += length;
+                break;
+            case INSIDE:
+                hiliteLength += length;
+            case AFTER:
+                break;
+        }
+    }
+
+    public void endCharHilite() throws SAXException {
+        assert hiliteState == HiliteState.INSIDE;
+        hiliteState = HiliteState.AFTER;
+    }
+
+    public void endRange() throws SAXException {
+        assert hiliteState == HiliteState.INSIDE;
+        hiliteState = HiliteState.AFTER;
+    }
+
+    public void endSource() throws SAXException {
+        handler.endString();
+    }
+
+    public void newLine() throws SAXException {
+        handler.characters(NEWLINE, 0, 1);
+        switch (hiliteState) {
+            case BEFORE:
+                hiliteStart++;
+                break;
+            case INSIDE:
+                hiliteLength++;
+            case AFTER:
+                break;
+        }
+    }
+
+    public void startCharHilite(int oneBasedLine, int oneBasedColumn)
+            throws SAXException {
+        assert hiliteState == HiliteState.BEFORE;
+        hiliteState = HiliteState.INSIDE;
+    }
+
+    public void startRange(int oneBasedLine, int oneBasedColumn)
+            throws SAXException {
+        assert hiliteState == HiliteState.BEFORE;
+        hiliteState = HiliteState.INSIDE;
+    }
+
+    public void startSource(String type, String encoding) throws SAXException {
+        hiliteStart = 0;
+        hiliteLength = 0;
+        hiliteState = HiliteState.BEFORE;
+        handler.startString();
+    }
+
+    /**
+     * Returns the hiliteLength.
+     * 
+     * @return the hiliteLength
+     */
+    public int getHiliteLength() {
+        return hiliteLength;
+    }
+
+    /**
+     * Returns the hiliteStart.
+     * 
+     * @return the hiliteStart
+     */
+    public int getHiliteStart() {
+        return hiliteStart;
+    }
+
+}
---
+++ src/nu/validator/messages/JsonMessageEmitter.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2007 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.messages;
+
+import nu.validator.json.JsonHandler;
+import nu.validator.messages.types.MessageType;
+import nu.validator.source.SourceHandler;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+public class JsonMessageEmitter extends MessageEmitter {
+
+    private final JsonHandler handler;
+    
+    private final String callback;
+    
+    private final JsonExtractHandler extractHandler;
+
+    private final JsonMessageTextHandler messageTextHandler;
+    
+    private boolean arrayOpen;
+    
+    /**
+     * @param handler
+     */
+    public JsonMessageEmitter(final JsonHandler handler, final String callback) {
+        this.handler = handler;
+        this.callback = callback;
+        this.extractHandler = new JsonExtractHandler(handler);
+        this.messageTextHandler = new JsonMessageTextHandler(handler);
+    }
+
+    @Override
+    public void endMessage() throws SAXException {
+        handler.endObject();
+    }
+
+    @Override
+    public void startMessage(MessageType type, String systemId,
+            int oneBasedFirstLine, int oneBasedFirstColumn,
+            int oneBasedLastLine, int oneBasedLastColumn, boolean exact)
+            throws SAXException {
+        assert arrayOpen;
+        handler.startObject();
+        handler.key("type");
+        handler.string(type.getSuperType());
+        if (systemId != null) {
+            handler.key("url");
+            handler.string(systemId);
+        }
+        if (oneBasedLastLine != -1) {
+            handler.key("lastLine");
+            handler.number(oneBasedLastLine);
+            if (oneBasedFirstLine != oneBasedLastLine) {
+                handler.key("firstLine");
+                handler.number(oneBasedFirstLine);                
+            }
+            if (oneBasedLastColumn != -1) {
+                handler.key("lastColumn");
+                handler.number(oneBasedLastColumn);                                
+                if (oneBasedFirstColumn != oneBasedLastColumn) {
+                    handler.key("firstColumn");
+                    handler.number(oneBasedFirstColumn);                
+                }        
+            }        
+        }
+        String subType = type.getSubType();
+        if (subType != null) {
+            handler.key("subType");
+            handler.string(subType);
+        }
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#endFullSource()
+     */
+    @Override
+    public void endFullSource() throws SAXException {
+        // TODO Auto-generated method stub
+        super.endFullSource();
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#endMessages()
+     */
+    @Override
+    public void endMessages() throws SAXException {
+        maybeCloseArray();
+        handler.endObject();
+        handler.endDocument();
+    }
+
+    /**
+     * @throws SAXException
+     */
+    private void maybeCloseArray() throws SAXException {
+        if (arrayOpen) {
+            handler.endArray();
+            arrayOpen = false;
+        }
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#endSource()
+     */
+    @Override
+    public void endSource() throws SAXException {
+        handler.key("hiliteStart");
+        handler.number(extractHandler.getHiliteStart());
+        handler.key("hiliteLength");
+        handler.number(extractHandler.getHiliteLength());
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#endText()
+     */
+    @Override
+    public void endText() throws SAXException {
+        handler.endString();
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#startFullSource()
+     */
+    @Override
+    public SourceHandler startFullSource() throws SAXException {
+        maybeCloseArray();
+        // TODO Auto-generated method stub
+        return super.startFullSource();
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#startMessages(java.lang.String)
+     */
+    @Override
+    public void startMessages(String documentUri) throws SAXException {
+        handler.startDocument(callback);
+        handler.startObject();
+        if (documentUri != null) {
+            handler.key("url");
+            handler.string(documentUri);
+        }
+        handler.key("messages");
+        handler.startArray();
+        arrayOpen = true;
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#startSource()
+     */
+    @Override
+    public SourceHandler startSource() throws SAXException {
+        handler.key("extract");
+        return extractHandler;
+    }
+
+    /**
+     * @see nu.validator.messages.MessageEmitter#startText()
+     */
+    @Override
+    public MessageTextHandler startText() throws SAXException {
+        handler.key("message");
+        handler.startString();
+        return messageTextHandler;
+    }
+
+}
