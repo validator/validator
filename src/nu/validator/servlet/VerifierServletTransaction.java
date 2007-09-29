6b377f7144f3627628a7a13c4483c825948c5fad
hsivonen@c6dcca6c-c432-0410-a79e-3fbb6554aff1 <hsivonen@c6dcca6c-c432-0410-a79e-3fbb6554aff1>
2007-09-29 13:17:38 +0000

    allow circular redirects
    --HG--
    extra : convert_revision : svn%3Ac6dcca6c-c432-0410-a79e-3fbb6554aff1/trunk%4081

--- src/nu/validator/servlet/VerifierServletTransaction.java
+++ src/nu/validator/servlet/VerifierServletTransaction.java
@@ -27,6 +27,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
