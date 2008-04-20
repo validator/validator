c54eca260e8b618bb5d4e271e223a4e0ffca85a4
hsivonen@e7398766-c432-0410-9565-638d39ca0058 <hsivonen@e7398766-c432-0410-9565-638d39ca0058>
2008-04-20 18:36:28 +0000

    add comment about outstanding feedback
    --HG--
    extra : convert_revision : svn%3Ae7398766-c432-0410-9565-638d39ca0058/trunk%40293

--- relaxng/datatype/java/src/org/whattf/datatype/DateOrTime.java
+++ relaxng/datatype/java/src/org/whattf/datatype/DateOrTime.java
@@ -34,7 +34,8 @@ public class DateOrTime extends AbstractDatetime {
      * The rexexp for this datatype.
      */
     private static final Pattern THE_PATTERN = Pattern.compile("^(?:(?:([0-9]{4,})-([0-9]{2})-([0-9]{2})(?:T([0-9]{2}):([0-9]{2})(?::([0-9]{2})(?:\\.[0-9]+)?)?(?:Z|(?:([+-][0-9]{2}):([0-9]{2})))?)?)|(?:([0-9]{2}):([0-9]{2})(?::([0-9]{2})(?:\\.[0-9]+)?)?(?:Z|(?:([+-][0-9]{2}):([0-9]{2})))?))$");
-
+    // XXX this is not per spec. outstanding feedback in Hixie's microformats-dates folder
+    
     private DateOrTime() {
         super();
     }
