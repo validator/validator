eede2031dbb61cd6ce0aad19a0b10302f3db93d3
James Clark <jjc@jclark.com>
2008-10-27 07:09:47 +0000

    Remove scripts directory

--- jing-trang/scripts/build-jing.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-set JING_HOME=%~d0%~p0..
-%JAVA_HOME%\bin\java -Dant.home=%JING_HOME% -cp %JING_HOME%\lib\ant.jar;%JING_HOME%\lib\crimson.jar;%JING_HOME%\lib\saxon.jar;%JING_HOME%\lib\optional.jar;%JAVA_HOME%\lib\tools.jar;%JING_HOME%\lib\regex.jar;%JING_HOME%\lib\regex2.jar;%JING_HOME%\lib\isorelax.jar org.apache.tools.ant.Main -buildfile %JING_HOME%\build.xml %*
