c1de397815b5d7c337cd85b9f8162376978af1d8
James Clark <jjc@jclark.com>
2008-11-03 09:12:32 +0000

    Set a URIResolver on the schematron transformerHandler.

--- jing-trang/mod/schematron/src/main/com/thaiopensource/validate/schematron/ValidatorImpl.java
+++ jing-trang/mod/schematron/src/main/com/thaiopensource/validate/schematron/ValidatorImpl.java
@@ -1,14 +1,17 @@
 package com.thaiopensource.validate.schematron;
 
 import com.thaiopensource.util.PropertyMap;
+import com.thaiopensource.validate.ResolverFactory;
 import com.thaiopensource.validate.ValidateProperty;
 import com.thaiopensource.validate.Validator;
+import com.thaiopensource.xml.sax.Resolver;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.DTDHandler;
 import org.xml.sax.ErrorHandler;
 
 import javax.xml.transform.Templates;
 import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.URIResolver;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.sax.TransformerHandler;
@@ -18,12 +21,14 @@ class ValidatorImpl implements Validator {
   private final SAXTransformerFactory factory;
   private final ContentHandler outputHandler;
   private TransformerHandler transformerHandler;
+  private final Resolver resolver;
 
   ValidatorImpl(Templates templates, SAXTransformerFactory factory, PropertyMap properties) {
     this.templates = templates;
     this.factory = factory;
     ErrorHandler eh = ValidateProperty.ERROR_HANDLER.get(properties);
     outputHandler = new OutputHandler(eh);
+    resolver = ResolverFactory.createResolver(properties);
     initTransformerHandler();
   }
 
@@ -42,8 +47,9 @@ class ValidatorImpl implements Validator {
   private void initTransformerHandler() {
     try {
       transformerHandler = factory.newTransformerHandler(templates);
-      // XXX set up transformer with a resolver that uses the resolver
-      // XXX and XMLReaderCreator from properties
+      URIResolver uriResolver = resolver.getUriResolver();
+      if (uriResolver != null)
+        transformerHandler.getTransformer().setURIResolver(uriResolver);
       // XXX set up transformer with an ErrorListener that just throws
       // XXX (what about errors from document() calls?)
     }
