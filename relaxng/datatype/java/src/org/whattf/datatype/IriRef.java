4aba0f58a2ad7cfe13fe0db42cb9a11ce9f085d0
Michael(tm) Smith <mike@w3.org>
2011-01-06 09:41:24 +0900

    fixed NullPointerException in non-empty IRI check

--- relaxng/datatype/java/src/org/whattf/datatype/IriRef.java
+++ relaxng/datatype/java/src/org/whattf/datatype/IriRef.java
@@ -249,15 +249,15 @@ public class IriRef extends AbstractDatatype {
         } catch (RhinoException e) {
             throw newDatatypeException(e.getMessage());
         }
-        if ("".equals(iri.toString())) {
-                throw newDatatypeException("Must be non-empty.");
-        }
         if (isAbsolute()) {
             if (iri != null && !iri.isAbsolute()) {
                 throw newDatatypeException("Not an absolute IRI.");
             }
         }
         if (iri != null) {
+            if ("".equals(iri.toString())) {
+                    throw newDatatypeException("Must be non-empty.");
+            }
             if (data) {
                 try {
                     DataUri dataUri = new DataUri(iri);
