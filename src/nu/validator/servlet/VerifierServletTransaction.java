3bc8ef7970d6b59009f8666d7518ad1ae6c2acaa
Henri Sivonen <hsivonen@iki.fi>
2011-05-25 14:29:57 +0300

    Bug 817 - Warn about features that are not widely implemented yet.

--- src/nu/validator/servlet/VerifierServletTransaction.java
+++ src/nu/validator/servlet/VerifierServletTransaction.java
@@ -233,7 +233,7 @@ class VerifierServletTransaction implements DocumentModeHandler, SchemaResolver 
             "http://c.validator.nu/text-content/",
             "http://c.validator.nu/unchecked/",
             "http://c.validator.nu/usemap/", "http://c.validator.nu/obsolete/",
-            "http://c.validator.nu/xml-pi/" };
+            "http://c.validator.nu/xml-pi/", "http://c.validator.nu/unsupported/" };
 
     private static final String[] ALL_CHECKERS_HTML4 = {
             "http://c.validator.nu/table/", "http://c.validator.nu/nfc/",
@@ -444,6 +444,8 @@ class VerifierServletTransaction implements DocumentModeHandler, SchemaResolver 
                     CheckerSchema.CONFORMING_BUT_OBSOLETE_WARNER);
             schemaMap.put("http://c.validator.nu/xml-pi/",
                     CheckerSchema.XML_PI_CHECKER);
+            schemaMap.put("http://c.validator.nu/unsupported/",
+                    CheckerSchema.UNSUPPORTED_CHECKER);
 
             for (int i = 0; i < presetUrls.length; i++) {
                 String[] urls1 = SPACE.split(presetUrls[i]);
