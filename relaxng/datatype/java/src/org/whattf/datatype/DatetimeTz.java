7350f0f6e0a45f0ab884fc0edc47be7fe327e9bf
Michael[tm] Smith <mike@w3.org>
2012-03-09 14:41:11 +0900

    Milliseconds are 1, 2, or 3 characters. b=912.

--- relaxng/datatype/java/src/org/whattf/datatype/DatetimeTz.java
+++ relaxng/datatype/java/src/org/whattf/datatype/DatetimeTz.java
@@ -48,7 +48,7 @@ public final class DatetimeTz extends AbstractDatetime {
     /**
      * The rexexp for this datatype.
      */
-    private static final Pattern THE_PATTERN = Pattern.compile("^([0-9]{4,})-([0-9]{2})-([0-9]{2})(T| )([0-9]{2}):([0-9]{2}):([0-9]{2})(?:\\.[0-9]+)?(?:Z|(?:([+-][0-9]{2}):([0-9]{2})))$");
+    private static final Pattern THE_PATTERN = Pattern.compile("^([0-9]{4,})-([0-9]{2})-([0-9]{2})(T| )([0-9]{2}):([0-9]{2}):([0-9]{2})(?:\\.[0-9]{1,3})?(?:Z|(?:([+-][0-9]{2}):([0-9]{2})))$");
 
     /**
      * Constructor.
