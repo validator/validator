66e31b85a59513cefd00ef5e9a0ce35e28d38419
Michael[tm] Smith <mike@w3.org>
2013-07-15 13:29:42 +0900

    Report an error for empty <title>. b=954

--- non-schema/java/src/org/whattf/checker/TextContentChecker.java
+++ non-schema/java/src/org/whattf/checker/TextContentChecker.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2006 Henri Sivonen
- * Copyright (c) 2010-2011 Mozilla Foundation
+ * Copyright (c) 2010-2013 Mozilla Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
@@ -54,6 +54,7 @@ public final class TextContentChecker extends Checker {
      * tail.
      */
     private final LinkedList<DatatypeStreamingValidator> stack = new LinkedList<DatatypeStreamingValidator>();
+    private boolean isEmpty;
 
     /**
      * Constructor.
@@ -104,6 +105,7 @@ public final class TextContentChecker extends Checker {
      */
     public void characters(char[] ch, int start, int length)
             throws SAXException {
+        isEmpty = false;
         for (DatatypeStreamingValidator dsv : stack) {
             if (dsv != null) {
                 dsv.addCharacters(ch, start, length);
@@ -117,6 +119,10 @@ public final class TextContentChecker extends Checker {
      */
     public void endElement(String uri, String localName, String qName)
             throws SAXException {
+        if (isEmpty && "http://www.w3.org/1999/xhtml".equals(uri)
+                && "title".equals(localName)) {
+            err("Element \u201Ctitle\u201d must not be empty.");
+        }
         DatatypeStreamingValidator dsv = stack.removeLast();
         if (dsv != null) {
             try {
@@ -217,6 +223,7 @@ public final class TextContentChecker extends Checker {
     public void startElement(String uri, String localName, String qName,
             Attributes atts) throws SAXException {
         stack.addLast(streamingValidatorFor(uri, localName, atts));
+        isEmpty = true;
     }
 
     /**
