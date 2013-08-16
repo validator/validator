00b782a5fbc154e08dedcf6f1fda1dcd579558c6
sideshowbarker <sideshowbarker@369101cc-9a96-11dd-8e58-870c635edf7a>
2013-08-16 00:20:51 +0000

    Don't emit "from namespace" with element names in error messages.

--- jing-trang/mod/rng-validate/src/main/com/thaiopensource/relaxng/impl/NameFormatter.java
+++ jing-trang/mod/rng-validate/src/main/com/thaiopensource/relaxng/impl/NameFormatter.java
@@ -7,9 +7,10 @@ public class NameFormatter {
   public static String format(Name name) {
     String localName = name.getLocalName();
     String namespaceUri = name.getNamespaceUri();
-    if (namespaceUri.equals(""))
-      return SchemaBuilderImpl.localizer.message("name_absent_namespace", localName);
-    else
-      return SchemaBuilderImpl.localizer.message("name_with_namespace", namespaceUri, localName);
+    return SchemaBuilderImpl.localizer.message("name_absent_namespace", localName);
+    // if (namespaceUri.equals(""))
+      // return SchemaBuilderImpl.localizer.message("name_absent_namespace", localName);
+    // else
+      // return SchemaBuilderImpl.localizer.message("name_with_namespace", namespaceUri, localName);
   }
 }
