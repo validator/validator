0bc936ffdb59d49c1b53c0e8c4e968b01db9b9d6
Michael[tm] Smith <mike@w3.org>
2014-02-28 00:48:36 +0900

    Exit the meta@name maintenance business.

--- relaxng/datatype/java/src/org/whattf/datatype/MetaName.java
+++ relaxng/datatype/java/src/org/whattf/datatype/MetaName.java
@@ -22,214 +22,31 @@
 
 package org.whattf.datatype;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashSet;
 
 import org.relaxng.datatype.DatatypeException;
 
 public class MetaName extends AbstractDatatype {
 
-    private static final String[] VALID_NAMES = {
-        "aglsterms.act", // extension
-        "aglsterms.accessibility", // extension
-        "aglsterms.accessmode", // extension
-        "aglsterms.aggregationlevel", // extension
-        "aglsterms.availability", // extension
-        "aglsterms.case", // extension
-        "aglsterms.category", // extension
-        "aglsterms.datelicensed", // extension
-        "aglsterms.documenttype", // extension
-        "aglsterms.function", // extension
-        "aglsterms.isbasisfor", // extension
-        "aglsterms.isbasedon", // extension
-        "aglsterms.jurisdiction", // extension
-        "aglsterms.mandate", // extension
-        "aglsterms.protectivemarking", // extension
-        "aglsterms.regulation", // extension
-        "aglsterms.servicetype", // extension
-        "alexaverifyid", // extension
-        "apple-mobile-web-app-capable", // extension
-        "apple-mobile-web-app-status-bar-style", // extension
-        "apple-mobile-web-app-title", // extension
-        "application-name",
-        "author",
-        "baiduspider", // extension
-        "bitcoin", // extension
-        "blazerr-secure", // extension
-        "blazerr-seo", // extension
-        "blazerr-ssl", // extension
-        "blazerr-support-id-noncookies", // extension
-        "blazerr-support-identifier", // extension
-        "bug.component", // extension
-        "bug.product", // extension
-        "bug.short_desc", // extension
-        "csrf-param", // extension
-        "csrf-token", // extension
-        "dc.date.issued", // extension
-        "dc.language", // extension
-        "dcterms.abstract", // extension
-        "dcterms.accessrights", // extension
-        "dcterms.accrualmethod", // extension
-        "dcterms.accrualperiodicity", // extension
-        "dcterms.accrualpolicy", // extension
-        "dcterms.alternative", // extension
-        "dcterms.audience", // extension
-        "dcterms.available", // extension
-        "dcterms.bibliographiccitation", // extension
-        "dcterms.conformsto", // extension
-        "dcterms.contributor", // extension
-        "dcterms.coverage", // extension
-        "dcterms.created", // extension
-        "dcterms.creator", // extension
-        "dcterms.date", // extension
-        "dcterms.dateaccepted", // extension
-        "dcterms.datecopyrighted", // extension
-        "dcterms.datesubmitted", // extension
-        "dcterms.description", // extension
-        "dcterms.educationlevel", // extension
-        "dcterms.extent", // extension
-        "dcterms.format", // extension
-        "dcterms.hasformat", // extension
-        "dcterms.haspart", // extension
-        "dcterms.hasversion", // extension
-        "dcterms.identifier", // extension
-        "dcterms.instructionalmethod", // extension
-        "dcterms.isformatof", // extension
-        "dcterms.ispartof", // extension
-        "dcterms.isreferencedby", // extension
-        "dcterms.isreplacedby", // extension
-        "dcterms.isrequiredby", // extension
-        "dcterms.issued", // extension
-        "dcterms.isversionof", // extension
-        "dcterms.language", // extension
-        "dcterms.license", // extension
-        "dcterms.mediator", // extension
-        "dcterms.medium", // extension
-        "dcterms.modified", // extension
-        "dcterms.provenance", // extension
-        "dcterms.publisher", // extension
-        "dcterms.references", // extension
-        "dcterms.relation", // extension
-        "dcterms.replaces", // extension
-        "dcterms.requires", // extension
-        "dcterms.rights", // extension
-        "dcterms.rightsholder", // extension
-        "dcterms.source", // extension
-        "dcterms.spatial", // extension
-        "dcterms.subject", // extension
-        "dcterms.tableofcontents", // extension
-        "dcterms.temporal", // extension
-        "dcterms.title", // extension
-        "dcterms.type", // extension
-        "dcterms.valid", // extension
-        "description",
-        "designer", // extension
-        "essaydirectory", // extension
-        "format-detection", // extension
-        "fragment", // extension
-        "generator",
-        "geo.a1", // extension
-        "geo.a2", // extension
-        "geo.a3", // extension
-        "geo.country", // extension
-        "geo.lmk", // extension
-        "geo.placename", // extension
-        "geo.position", // extension
-        "geo.region", // extension
-        "globrix.bathrooms", // extension
-        "globrix.bedrooms", // extension
-        "globrix.condition", // extension
-        "globrix.features", // extension
-        "globrix.instruction", // extension
-        "globrix.latitude", // extension
-        "globrix.longitude ", // extension
-        "globrix.outsidespace", // extension
-        "globrix.parking", // extension
-        "globrix.period", // extension
-        "globrix.poa", // extension
-        "globrix.postcode", // extension
-        "globrix.price", // extension
-        "globrix.priceproximity", // extension
-        "globrix.tenure", // extension
-        "globrix.type", // extension
-        "globrix.underoffer", // extension
-        "google-site-verification", // extension
-        "googlebot", // extension
-        "handheldfriendly", // extension
-        "icbm", // extension
-        "itemsperpage", // extension
-        "keywords",
-        "meta_date", // extension
-        "mobile-web-app-capable", // extension
-        "mobileoptimized", // extension
-        "msapplication-config", // extension
-        "msapplication-navbutton-color", // extension
-        "msapplication-square150x150logo", // extension
-        "msapplication-square310x310logo", // extension
-        "msapplication-square70x70logo", // extension
-        "msapplication-starturl", // extension
-        "msapplication-task", // extension
-        "msapplication-tilecolor", // extension
-        "msapplication-tileimage", // extension
-        "msapplication-tooltip", // extension
-        "msapplication-wide310x150logo", // extension
-        "msapplication-window", // extension
-        "msvalidate.01", // extension
-        "norton-safeweb-site-verification", // extension
-        "p:domain_verify", // extension
-        "rating", // extension
-        "referrer", // extension
-        "review_date", // extension
-        "revisit-after", // extension
-        "rights-standard", // extension
-        "robots", // extension
-        "skype_toolbar", // extension
-        "slurp", // extension
-        "startindex", // extension
-        "startver", // extension
-        "teoma", // extension
-        "twitter:app:country ", // extension
-        "twitter:app:id:googleplay", // extension
-        "twitter:app:id:ipad ", // extension
-        "twitter:app:id:iphone", // extension
-        "twitter:app:url:googleplay", // extension
-        "twitter:app:url:ipad", // extension
-        "twitter:app:url:iphone", // extension
-        "twitter:card", // extension
-        "twitter:creator", // extension
-        "twitter:creator:id", // extension
-        "twitter:description", // extension
-        "twitter:domain", // extension
-        "twitter:image", // extension
-        "twitter:image0", // extension
-        "twitter:image1", // extension
-        "twitter:image2", // extension
-        "twitter:image3", // extension
-        "twitter:image:height", // extension
-        "twitter:image:src", // extension
-        "twitter:image:width", // extension
-        "twitter:site", // extension
-        "twitter:site:id", // extension
-        "twitter:title", // extension
-        "twitter:url", // extension
-        "verify-v1", // extension
-        "viewport", // extension
-        "wot-verification", // extension
-        "wt.ac", // extension
-        "wt.ad", // extension
-        "wt.cg_n", // extension
-        "wt.cg_s", // extension
-        "wt.mc_id", // extension
-        "wt.si_p", // extension
-        "wt.sv", // extension
-        "wt.ti", // extension
-        "y_key", // extension
-        "yandex-verification", // extension
-        "zoomcategory", // extension
-        "zoomimage", // extension
-        "zoompageboost", // extension
-        "zoomtitle", // extension
-        "zoomwords" // extension
-    };
+    private static final HashSet<String> registeredMetaNames = new HashSet<String>();
+
+    static {
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                MetaName.class.getClassLoader().getResourceAsStream(
+                        "nu/validator/localentities/files/meta-extensions")));
+        try {
+            String read = br.readLine();
+            while (read != null) {
+                registeredMetaNames.add(br.readLine());
+                read = br.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * The singleton instance.
@@ -246,7 +63,7 @@ public class MetaName extends AbstractDatatype {
     @Override public void checkValid(CharSequence literal)
             throws DatatypeException {
         String token = toAsciiLowerCase(literal);
-        if (Arrays.binarySearch(VALID_NAMES, token) < 0) {
+        if (!registeredMetaNames.contains(token)) {
             throw newDatatypeException("Keyword ", token, " is not registered.");
         }
     }
