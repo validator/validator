bf40b1dfdb1cb2f9477df1c79743cca246da0798
Michael[tm] Smith <mike@w3.org>
2014-06-18 22:05:39 +0900

    Checkpointing srcset datatype checking.

--- relaxng/datatype/java/src/org/whattf/datatype/ImageCandidateStrings.java
+++ relaxng/datatype/java/src/org/whattf/datatype/ImageCandidateStrings.java
@@ -22,17 +22,21 @@
 
 package org.whattf.datatype;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.relaxng.datatype.DatatypeException;
 
 public class ImageCandidateStrings extends AbstractDatatype {
 
-    private enum State {
-        SPLITTING_LOOP, URL, DESCRIPTOR
+    private static enum State {
+        SPLITTING_LOOP, URL, DESCRIPTORS
     }
 
-    /**
-     * The singleton instance.
-     */
+    private static final int EXTRACT_LIMIT = 15;
+
+    private static final ImageCandidateURL IC_URL = ImageCandidateURL.THE_INSTANCE;
+
     public static final ImageCandidateStrings THE_INSTANCE = new ImageCandidateStrings();
 
     protected ImageCandidateStrings() {
@@ -44,47 +48,72 @@ public class ImageCandidateStrings extends AbstractDatatype {
         if (literal.length() == 0) {
             err("Must contain one or more image candidate strings.");
         }
-        boolean lacksURL = true;
+        List<CharSequence> urls = new ArrayList<CharSequence>();
+        List<Integer> widths = new ArrayList<Integer>();
+        List<Integer> densities = new ArrayList<Integer>();
+        boolean eof = false;
+        boolean waitingForCandidate = true;
         int position = 0;
+        int start = 0;
         State state = State.SPLITTING_LOOP;
         for (int i = 0; i < literal.length(); i++) {
             char c = literal.charAt(i);
+            eof = i == literal.length() - 1;
             switch (state) {
                 case SPLITTING_LOOP:
                     if (isWhitespace(c)) {
                         position = i + 1;
                         continue;
                     } else if (',' == c) {
-                        if (lacksURL) {
-                            err("Found empty image candidate string.");
+                        if (urls.isEmpty()) {
+                            err("Starts with empty image-candidate string.");
                         }
-                        commaHandler(literal.subSequence(position, i + 1));
-                        lacksURL = true;
+                        if (waitingForCandidate) {
+                            errEmpty(literal.subSequence(start, i + 1));
+                        }
+                        commaHandler(literal.subSequence(start, i + 1));
+                        waitingForCandidate = true;
                         position = i + 1;
                         continue;
                     }
+                    start = position;
                     // fall through
                 case URL:
-                    lacksURL = false;
-                    if (i == literal.length() - 1 || isWhitespace(c)) {
+                    waitingForCandidate = false;
+                    if (eof || isWhitespace(c)) {
                         int end = isWhitespace(c) ? i : i + 1;
                         CharSequence url = literal.subSequence(position, end);
                         position = end;
-                        state = State.DESCRIPTOR;
+                        state = State.DESCRIPTORS;
                         if (endsWithComma(url)) {
-                            lacksURL = true;
+                            url = url.subSequence(0, url.length() - 1);
                             position = i + 1;
+                            waitingForCandidate = true;
                             state = State.SPLITTING_LOOP;
                         }
+                        if (eof || waitingForCandidate) {
+                            if (densities.indexOf(1) != -1) {
+                                errSameDensity(url,
+                                        urls.get(densities.indexOf(1)));
+                            }
+                            widths.add(null);
+                            densities.add(1);
+                        }
+                        IC_URL.checkValid(url);
+                        urls.add(url);
                         continue;
                     } else {
                         state = State.URL;
                         continue;
                     }
-                case DESCRIPTOR:
+                case DESCRIPTORS:
                     position = i + 1;
+                    if (i == literal.length() || ',' == c) {
+                      // add width
+                      // add density
+                    }
                     if (',' == c) {
-                        lacksURL = true;
+                        waitingForCandidate = true;
                         state = State.SPLITTING_LOOP;
                         continue;
                     } else {
@@ -92,8 +121,8 @@ public class ImageCandidateStrings extends AbstractDatatype {
                     }
             }
         }
-        if (lacksURL) {
-            err("Found empty image candidate string.");
+        if (waitingForCandidate) {
+            err("Ends with empty image-candidate string.");
         }
     }
 
@@ -105,22 +134,33 @@ public class ImageCandidateStrings extends AbstractDatatype {
             if (',' != cs.charAt(i)) {
                 break;
             }
-            err("Found empty image candidate string.");
+            errEmpty(cs);
         }
         return cs.length() - 1;
     }
 
     private boolean endsWithComma(CharSequence cs) throws DatatypeException {
         int end = commaHandler(cs);
-        Html5DatatypeLibrary dl = new Html5DatatypeLibrary();
-        IriRef url = (IriRef) dl.createDatatype("iri-ref");
-        System.out.println(cs.subSequence(0, end));
-        url.checkValid(cs.subSequence(0, end));
         return end == cs.length() - 1;
     }
 
     private void err(String message) throws DatatypeException {
         throw newDatatypeException(message);
+    }
+
+    private void errEmpty(CharSequence extract) throws DatatypeException {
+        int len = extract.length();
+        if (len > EXTRACT_LIMIT) {
+            extract = "\u2026" + extract.subSequence(len - EXTRACT_LIMIT, len);
+        }
+        err("Empty image-candidate string at \u201c" + extract + "\u201d.");
+    }
+
+    private void errSameDensity(CharSequence url1, CharSequence url2)
+            throws DatatypeException {
+        err("Density for image \u201c" + url1
+                + "\u201d is identical to density for image \u201c" + url2
+                + "\u201d.");
     }
 
     protected boolean widthRequired() {
