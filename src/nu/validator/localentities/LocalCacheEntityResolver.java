629bc69de6e231ee490e7601f743f9d6691a4da1
Bob Hogg <hogg.bob.2@gmail.com>
2015-03-03 00:08:14 -0800

    Removes all unused imports from the Java files in the src/ directory
    Signed-off-by: Bob Hogg <hogg.bob.2@gmail.com>

--- src/nu/validator/localentities/LocalCacheEntityResolver.java
+++ src/nu/validator/localentities/LocalCacheEntityResolver.java
@@ -1,8 +1,6 @@
 package nu.validator.localentities;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
