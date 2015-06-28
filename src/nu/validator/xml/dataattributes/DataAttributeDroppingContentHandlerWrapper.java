a21f1ad7d86d04ce12fe66a3b6f3bd0a782edd70
Michael[tm] Smith <mike@w3.org>
2015-06-28 17:53:38 +0900

    Minor code-style tweaks.

--- src/nu/validator/xml/dataattributes/DataAttributeDroppingContentHandlerWrapper.java
+++ src/nu/validator/xml/dataattributes/DataAttributeDroppingContentHandlerWrapper.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2008-2011 Mozilla Foundation
  *
- * Permission is hereby granted, free of charge, to any person obtaining a 
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in 
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
 
@@ -33,28 +33,28 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 /**
- * This wrapper removes <code>data-*</code> attributes from the pipeline and emits errors if the 
- * <code>data-*</code> attributes contain prohibited characters.
- * 
- * @version $Id$
- * @author hsivonen
+ * This wrapper removes <code>data-*</code> attributes from the pipeline and
+ * emits errors if the <code>data-*</code> attributes contain prohibited
+ * characters.
  */
-public class DataAttributeDroppingContentHandlerWrapper implements ContentHandler {
+public class DataAttributeDroppingContentHandlerWrapper implements
+        ContentHandler {
 
     private final ContentHandler delegate;
-    
+
     private final ErrorHandler errorHandler;
 
     private Locator locator = null;
-    
+
     /**
      * @param delegate
      */
-    public DataAttributeDroppingContentHandlerWrapper(ContentHandler delegate, ErrorHandler errorHandler) {
+    public DataAttributeDroppingContentHandlerWrapper(ContentHandler delegate,
+            ErrorHandler errorHandler) {
         this.delegate = delegate;
         this.errorHandler = errorHandler;
     }
-    
+
     /**
      * @param arg0
      * @param arg1
@@ -79,7 +79,8 @@ public class DataAttributeDroppingContentHandlerWrapper implements ContentHandle
      * @param arg1
      * @param arg2
      * @throws SAXException
-     * @see org.xml.sax.ContentHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+     * @see org.xml.sax.ContentHandler#endElement(java.lang.String,
+     *      java.lang.String, java.lang.String)
      */
     public void endElement(String arg0, String arg1, String arg2)
             throws SAXException {
@@ -111,7 +112,8 @@ public class DataAttributeDroppingContentHandlerWrapper implements ContentHandle
      * @param arg0
      * @param arg1
      * @throws SAXException
-     * @see org.xml.sax.ContentHandler#processingInstruction(java.lang.String, java.lang.String)
+     * @see org.xml.sax.ContentHandler#processingInstruction(java.lang.String,
+     *      java.lang.String)
      */
     public void processingInstruction(String arg0, String arg1)
             throws SAXException {
@@ -123,7 +125,7 @@ public class DataAttributeDroppingContentHandlerWrapper implements ContentHandle
      * @see org.xml.sax.ContentHandler#setDocumentLocator(org.xml.sax.Locator)
      */
     public void setDocumentLocator(Locator arg0) {
-        locator  = arg0;
+        locator = arg0;
         delegate.setDocumentLocator(arg0);
     }
 
@@ -150,36 +152,44 @@ public class DataAttributeDroppingContentHandlerWrapper implements ContentHandle
      * @param arg2
      * @param attributes
      * @throws SAXException
-     * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+     * @see org.xml.sax.ContentHandler#startElement(java.lang.String,
+     *      java.lang.String, java.lang.String, org.xml.sax.Attributes)
      */
     public void startElement(String ns, String arg1, String arg2,
             Attributes attributes) throws SAXException {
         if ("http://www.w3.org/1999/xhtml" == ns) {
-            delegate.startElement(ns, arg1, arg2, filterAttributes(attributes));            
+            delegate.startElement(ns, arg1, arg2, filterAttributes(attributes));
         } else {
             delegate.startElement(ns, arg1, arg2, attributes);
         }
     }
 
-    private Attributes filterAttributes(Attributes attributes) throws SAXException {
+    private Attributes filterAttributes(Attributes attributes)
+            throws SAXException {
         int len = attributes.getLength();
         for (int i = 0; i < len; i++) {
             String local = attributes.getLocalName(i);
-            if (local.length() > 5 && local.startsWith("data-") && attributes.getURI(i) == "") {
+            if (local.length() > 5 && local.startsWith("data-")
+                    && attributes.getURI(i) == "") {
                 if (errorHandler != null) {
                     checkDataName(local);
                 }
                 AttributesImpl attributesImpl = new AttributesImpl();
                 for (int j = 0; j < i; j++) {
-                    attributesImpl.addAttribute(attributes.getURI(j), attributes.getLocalName(j), attributes.getQName(j), attributes.getType(j), attributes.getValue(j));
+                    attributesImpl.addAttribute(attributes.getURI(j),
+                            attributes.getLocalName(j), attributes.getQName(j),
+                            attributes.getType(j), attributes.getValue(j));
                 }
                 for (int k = i + 1; k < len; k++) {
                     String uri = attributes.getURI(k);
                     local = attributes.getLocalName(k);
-                    if (local.length() > 5 && local.startsWith("data-") && "" == uri) {
+                    if (local.length() > 5 && local.startsWith("data-")
+                            && "" == uri) {
                         checkDataName(local);
                     } else {
-                        attributesImpl.addAttribute(uri, local, attributes.getQName(k), attributes.getType(k), attributes.getValue(k));                        
+                        attributesImpl.addAttribute(uri, local,
+                                attributes.getQName(k), attributes.getType(k),
+                                attributes.getValue(k));
                     }
                 }
                 return attributesImpl;
@@ -192,9 +202,16 @@ public class DataAttributeDroppingContentHandlerWrapper implements ContentHandle
         for (int i = 5; i < local.length(); i++) {
             char c = local.charAt(i);
             if (c >= 'A' && c <= 'Z') {
-                errorHandler.error(new SAXParseException("\u201Cdata-*\u201D attributes must not have characters from the range \u201CA\u201D\u2026\u201CZ\u201D in the name.", locator));
+                errorHandler.error(new SAXParseException(
+                        "\u201Cdata-*\u201D attributes must not have"
+                        + " characters from the range"
+                        + " \u201CA\u201D\u2026\u201CZ\u201D in the name.",
+                        locator));
             } else if (!NCName.isNCNameTrail(c)) {
-                errorHandler.error(new SAXParseException("\u201Cdata-*\u201D attribute names must be XML 1.0 4th ed. plus Namespaces NCNames.", locator));                
+                errorHandler.error(new SAXParseException(
+                        "\u201Cdata-*\u201D attribute names must be"
+                        + " XML 1.0 4th ed. plus Namespaces NCNames.",
+                        locator));
             }
         }
     }
@@ -203,13 +220,12 @@ public class DataAttributeDroppingContentHandlerWrapper implements ContentHandle
      * @param arg0
      * @param arg1
      * @throws SAXException
-     * @see org.xml.sax.ContentHandler#startPrefixMapping(java.lang.String, java.lang.String)
+     * @see org.xml.sax.ContentHandler#startPrefixMapping(java.lang.String,
+     *      java.lang.String)
      */
     public void startPrefixMapping(String arg0, String arg1)
             throws SAXException {
         delegate.startPrefixMapping(arg0, arg1);
     }
 
-    
-    
 }
