59d14095606868c6b7f90db0e1063eafea389288
Michael[tm] Smith <mike@w3.org>
2015-12-26 17:13:26 +0900

    Make "integrity" checking more helpful
    Refines https://github.com/validator/validator/issues/151

--- src/nu/validator/datatype/Html5DatatypeLibrary.java
+++ src/nu/validator/datatype/Html5DatatypeLibrary.java
@@ -178,6 +178,8 @@ public class Html5DatatypeLibrary implements DatatypeLibrary {
             return ImageCandidateStringsWidthRequired.THE_INSTANCE;
         } else if ("image-candidate-url".equals(typeLocalName)) {
             return ImageCandidateURL.THE_INSTANCE;
+        } else if ("integrity-metadata".equals(typeLocalName)) {
+            return IntegrityMetadata.THE_INSTANCE;
         }
         throw new DatatypeException("Unknown local name for datatype: " + typeLocalName);
     }
---
+++ src/nu/validator/datatype/IntegrityMetadata.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.datatype;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.relaxng.datatype.DatatypeException;
+
+public final class IntegrityMetadata extends AbstractDatatype {
+
+    /**
+     * The singleton instance.
+     */
+    public static final IntegrityMetadata THE_INSTANCE = new IntegrityMetadata();
+
+    private static final Pattern THE_PATTERN = Pattern.compile(
+            "^(?:sha256-|sha384-|sha512)(.+$)", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern FULL_PATTERN = Pattern.compile(
+            "^(?:sha256-|sha384-|sha512-)(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$",
+            Pattern.CASE_INSENSITIVE);
+
+    private IntegrityMetadata() {
+        super();
+    }
+
+    private final static boolean WARN = System.getProperty(
+            "nu.validator.datatype.warn", "").equals("true") ? true : false;
+
+    public void checkValid(CharSequence literal) throws DatatypeException {
+        Matcher m = getPattern().matcher(literal);
+        if (m.matches()) {
+            Matcher n = getFullPattern().matcher(literal);
+            if (WARN && !n.matches()) {
+                throw newDatatypeException("The text \u201c" + m.group(1)
+                        + "\u201d does not appear to be a valid"
+                        + " base64-encoded string.", WARN);
+            }
+        } else {
+            throw newDatatypeException("The value must start with"
+                    + " \u201csha256-\u201d or \u201csha384-\u201d"
+                    + " or \u201csha512-\u201d.");
+        }
+    }
+
+    /**
+     * Returns the regexp for this datatype.
+     *
+     * @return the base regexp for this datatype
+     * @see nu.validator.datatype.AbstractDatetime#getPattern()
+     */
+    protected Pattern getPattern() {
+        return THE_PATTERN;
+    }
+
+    /**
+     * Returns the full regexp for this datatype.
+     *
+     * @return the full regexp for this datatype
+     * @see nu.validator.datatype.AbstractDatetime#getPattern()
+     */
+    protected Pattern getFullPattern() {
+        return FULL_PATTERN;
+    }
+
+    @Override public String getName() {
+        return "integrity-metadata";
+    }
+}
