validator/validator

commit eea65a7dd837c39bd3961a3bf2326f276b587f9e

Allow rel=pingback|prefetch|stylesheet etc in body
See https://github.com/whatwg/html/pull/616 and
https://github.com/whatwg/html/commit/179983e9eb99efe417349a40ebb664bd11668ddd

Fixes https://github.com/validator/validator/issues/253

src/nu/validator/checker/schematronequiv/Assertions.java
@@ -361,7 +361,7 @@ public class Assertions extends Checker {
                 "th", "col", "colgroup", "pre" });
     }
 
-    private static final String[] SPECIAL_ANCESTORS = { "a", "address",
+    private static final String[] SPECIAL_ANCESTORS = { "a", "address", "body",
             "button", "caption", "dfn", "dt", "figcaption", "figure", "footer",
             "form", "header", "label", "map", "noscript", "th", "time",
             "progress", "meter", "article", "section", "aside", "nav", "h1",
@@ -471,6 +471,8 @@ public class Assertions extends Checker {
         registerProhibitedAncestor("footer", "main");
         registerProhibitedAncestor("nav", "main");
     }
+
+    private static final int BODY_MASK = (1 << specialAncestorNumber("body"));
 
     private static final int A_BUTTON_MASK = (1 << specialAncestorNumber("a"))
             | (1 << specialAncestorNumber("button"));
@@ -2174,6 +2176,7 @@ public class Assertions extends Checker {
                 }
             }
             if ("link" == localName) {
+                String relVal = atts.getValue("", "rel");
                 if (atts.getIndex("", "as") > -1
                         && atts.getIndex("", "rel") > -1
                         && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
@@ -2182,6 +2185,33 @@ public class Assertions extends Checker {
                             + " \u201Cas\u201D attribute must have a"
                             + " \u201Crel\u201D attribute with the value"
                             + " \u201Cpreload\u201D.");
+                }
+                if ((ancestorMask & BODY_MASK) != 0
+                        && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
+                                "dns-prefetch", relVal)
+                        && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
+                                "pingback", relVal)
+                        && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
+                                "preconnect", relVal)
+                        && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
+                                "prefetch", relVal)
+                        && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
+                                "prerender", relVal)
+                        && !lowerCaseLiteralEqualsIgnoreAsciiCaseString(
+                                "stylesheet", relVal)
+                        && atts.getIndex("", "itemprop") < 0
+                        && atts.getIndex("", "property") < 0) {
+                    err("A \u201Clink\u201D element must not appear"
+                            + " as a descendant of a \u201Cbody\u201D element"
+                            + " unless the \u201Clink\u201D element has an"
+                            + " \u201Citemprop\u201D attribute or has a"
+                            + " \u201Crel\u201D attribute whose value is one of"
+                            + " \u201Cdns-prefetch\u201D,"
+                            + " \u201Cpingback\u201D,"
+                            + " \u201Cpreconnect\u201D,"
+                            + " \u201Cprefetch\u201D,"
+                            + " \u201Cprerender\u201D, or"
+                            + " \u201Cstylesheet\u201D.");
                 }
             }
 

schema/html5/meta.rnc
@@ -209,6 +209,8 @@ datatypes w = "http://whattf.org/datatype-draft"
 		( empty )
 		
 	common.elem.metadata |= link.elem
+	common.elem.phrasing |= link.elem
+	common.elem.flow |= link.elem
 
 ## Global Style: <style>
 

commit c3bbcca1eb4c0b5c8937f25c24af9ad4c9c6538d

Fix regression caused by eea65a7

src/nu/validator/checker/schematronequiv/Assertions.java
@@ -499,7 +499,7 @@ public class Assertions extends Checker {
 
     private static final int HREF_MASK = (1 << 30);
 
-    private static final int LABEL_FOR_MASK = (1 << 28);
+    private static final int LABEL_FOR_MASK = (1 << 29);
 
     private static final Map<String, Set<String>> REQUIRED_ROLE_ANCESTOR_BY_DESCENDANT = new HashMap<>();
 