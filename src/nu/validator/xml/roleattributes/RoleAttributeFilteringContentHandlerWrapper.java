7c18e06a78f55174327508ac2a25fb5a2256f9f6
Shane McCarron <halindrome@gmail.com>
2016-07-07 10:07:38 -0500

    Added new roles from ARIA 1.1 and DPUB-ARIA
    Signed-off-by: Shane McCarron <halindrome@gmail.com>

--- src/nu/validator/xml/roleattributes/RoleAttributeFilteringContentHandlerWrapper.java
+++ src/nu/validator/xml/roleattributes/RoleAttributeFilteringContentHandlerWrapper.java
@@ -183,7 +183,22 @@ public class RoleAttributeFilteringContentHandlerWrapper implements
             "radiogroup", "region", "row", "rowgroup", "rowheader",
             "scrollbar", "search", "separator", "slider", "spinbutton",
             "status", "tab", "tablist", "tabpanel", "textbox", "timer",
-            "toolbar", "tooltip", "tree", "treegrid", "treeitem" };
+            "toolbar", "tooltip", "tree", "treegrid", "treeitem",
+            /* New non-abstract roles from ARIA 1.1 */
+            "cell", "feed", "figure", "none", "radiogroup", "searchbox",
+            "switch", "table", "term",
+            /* New non-abstract roles from ARIA-DPUB */
+            "doc-abstract", "doc-acknowledgments", "doc-afterword", "doc-appendix",
+            "doc-backlink", "doc-biblioentry", "doc-bibliography", "doc-biblioref",
+            "doc-chapter", "doc-colophon", "doc-conclusion", "doc-cover",
+            "doc-credit", "doc-credits", "doc-dedication", "doc-endnote",
+            "doc-endnotes", "doc-epigraph", "doc-epilogue", "doc-errata",
+            "doc-example", "doc-footnote", "doc-foreword", "doc-glossary",
+            "doc-glossref", "doc-index", "doc-introduction", "doc-noteref",
+            "doc-notice", "doc-pagebreak", "doc-pagelist", "doc-part", "doc-preface",
+            "doc-prologue", "doc-pullquote", "doc-qna", "doc-subtitle",
+            "doc-tip", "doc-toc"
+    };
 
     private Attributes filterAttributes(Attributes attributes)
             throws SAXException {
