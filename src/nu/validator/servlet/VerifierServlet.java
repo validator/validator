b6cad1c9f1e3b42efa6a80eb84612eae7b4522f6
Michael[tm] Smith <mike@w3.org>
2016-12-06 01:53:39 +0900

    Respond 202 if request lacks User-Agent header
    Responding with 400 seems to cause some problems in the validator.w3.org
    environment.
    
    Addresses https://github.com/validator/validator/issues/421

--- src/nu/validator/servlet/VerifierServlet.java
+++ src/nu/validator/servlet/VerifierServlet.java
@@ -257,7 +257,7 @@ public class VerifierServlet extends HttpServlet {
             return;
         }
         if (ua == null) {
-            response.sendError(400, "Bad Request. User-Agent header is missing.");
+            response.sendError(202, "Request accepted but lacks User-Agent header, so not processed.");
             return;
         }
         if (!isOptions) {
