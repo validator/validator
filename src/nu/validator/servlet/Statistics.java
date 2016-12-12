4da21549a9c1e9adbf5de9c6a57c8a846c37b9aa
Michael[tm] Smith <mike@w3.org>
2016-12-12 14:02:52 +0900

    Minor statistics-formatting tweak

--- src/nu/validator/servlet/Statistics.java
+++ src/nu/validator/servlet/Statistics.java
@@ -99,8 +99,8 @@ public class Statistics {
         INPUT_FILE_UPLOAD("\u2514 File-upload input"), //
         INPUT_ENTITY_BODY("\u2514 Entity-body input"), //
         INPUT_HTML("Input: text/html"), //
-        INPUT_XML("Input: application/xhtml+xml or other XML content type"), //
         INPUT_UNSUPPORTED("Input: unsupported content type (error)"), //
+        INPUT_XML("Input: application/xhtml+xml or other XML content type"), //
         OUTPUT_XML("Output: XML"), //
         OUTPUT_HTML("Output: HTML"), //
         OUTPUT_JSON("Output: JSON"), //
