validator/validator

commit 0789dc8b03bee75ba82f12af20b9785a161557bc

Stop hiding “sectioning roots” headings in outline
This change drops the special-casing of “sectioning roots” from the
outline feature, which had caused headings in blockquote, details,
fieldset, figure, and td elements to be hidden from the outline

src/nu/validator/servlet/OutlineBuildingXMLReaderWrapper.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012 Vadim Zaslawski, Ontos AG
- * Copyright (c) 2012-2016 Mozilla Foundation
+ * Copyright (c) 2012-2017 Mozilla Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -74,8 +74,7 @@ public final class OutlineBuildingXMLReaderWrapper implements XMLReader,
     private static final String[] SECTIONING_CONTENT_ELEMENTS = { "article",
             "aside", "nav", "section" };
 
-    private static final String[] SECTIONING_ROOT_ELEMENTS = { "blockquote",
-            "body", "details", "fieldset", "figure", "td" };
+    private static final String[] SECTIONING_ROOT_ELEMENTS = { "body" };
 
     private static final String[] H1_TO_H6_ELEMENTS = { "h1", "h2", "h3", "h4",
             "h5", "h6" };

commit b7956ea4a6231fa9f4e45dbb648828be8a37480c

(Re)hide sectioning roots in “Structural outline”
0789dc8 caused headings for sectioning roots to be shown in both the
“Heading-level outline” and “Structural outline” output. It’s correct to
show them in the “Heading-level outline”, but it’s not correct to show
them in the “Structural outline”, which attempts to show an outline view
based on the HTML outline algorithm.

So this change restores the “Structural outline” view to conformance
with the HTML spec, by not including headings for sectioning roots in
the outline, as the spec requires.

However, the “Heading-level outline” (which isn’t intended to conform to
the HTML outline algorithm) continues to show those headings.

src/nu/validator/servlet/OutlineBuildingXMLReaderWrapper.java
@@ -33,6 +33,7 @@ package nu.validator.servlet;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.regex.Pattern;
@@ -60,12 +61,27 @@ public final class OutlineBuildingXMLReaderWrapper implements XMLReader,
 
     private ContentHandler contentHandler;
 
+    private final ArrayList<String> SECTIONING_ROOT_ELEMENTS = new ArrayList<>();
+
+    private boolean isHeadingOutline = false;
+
     public OutlineBuildingXMLReaderWrapper(XMLReader wrappedReader,
-            HttpServletRequest request) {
+            HttpServletRequest request, boolean isHeadingOutline) {
         this.request = request;
+        this.isHeadingOutline = isHeadingOutline;
         this.wrappedReader = wrappedReader;
         this.contentHandler = wrappedReader.getContentHandler();
         this.inHgroup = false;
+        if (isHeadingOutline) {
+            this.SECTIONING_ROOT_ELEMENTS.add("body");
+        } else {
+            this.SECTIONING_ROOT_ELEMENTS.add("blockquote");
+            this.SECTIONING_ROOT_ELEMENTS.add("body");
+            this.SECTIONING_ROOT_ELEMENTS.add("details");
+            this.SECTIONING_ROOT_ELEMENTS.add("fieldset");
+            this.SECTIONING_ROOT_ELEMENTS.add("figure");
+            this.SECTIONING_ROOT_ELEMENTS.add("td");
+        }
         wrappedReader.setContentHandler(this);
     }
 
@@ -73,8 +89,6 @@ public final class OutlineBuildingXMLReaderWrapper implements XMLReader,
 
     private static final String[] SECTIONING_CONTENT_ELEMENTS = { "article",
             "aside", "nav", "section" };
-
-    private static final String[] SECTIONING_ROOT_ELEMENTS = { "body" };
 
     private static final String[] H1_TO_H6_ELEMENTS = { "h1", "h2", "h3", "h4",
             "h5", "h6" };
@@ -465,7 +479,8 @@ public final class OutlineBuildingXMLReaderWrapper implements XMLReader,
                     currentSection.sections.add(section);
                 }
             }
-        } else if (Arrays.binarySearch(SECTIONING_ROOT_ELEMENTS, localName) > -1) {
+        } else if (Arrays.binarySearch(SECTIONING_ROOT_ELEMENTS.toArray(),
+                localName) > -1) {
             // When exiting a sectioning root element, if the stack is not empty
             if (!outlineStack.isEmpty()) {
                 // Run these steps:
@@ -583,7 +598,8 @@ public final class OutlineBuildingXMLReaderWrapper implements XMLReader,
         // When entering a sectioning content element or a sectioning root
         // element
         if (Arrays.binarySearch(SECTIONING_CONTENT_ELEMENTS, localName) > -1
-                || Arrays.binarySearch(SECTIONING_ROOT_ELEMENTS, localName) > -1) {
+                || Arrays.binarySearch(SECTIONING_ROOT_ELEMENTS.toArray(),
+                        localName) > -1) {
             if (currentOutlinee != null) {
                 // If current outlinee is not null, and the current section has
                 // no heading,
@@ -720,9 +736,15 @@ public final class OutlineBuildingXMLReaderWrapper implements XMLReader,
             return;
         }
         if (currentOutlinee != null) {
-            request.setAttribute(
-                    "http://validator.nu/properties/document-outline",
-                    currentOutlinee.outline);
+            if (isHeadingOutline) {
+                request.setAttribute(
+                        "http://validator.nu/properties/heading-outline",
+                        currentOutlinee.outline);
+            } else {
+                request.setAttribute(
+                        "http://validator.nu/properties/document-outline",
+                        currentOutlinee.outline);
+            }
             setOutline(currentOutlinee.outline);
         }
         contentHandler.endDocument();

src/nu/validator/servlet/VerifierServletTransaction.java
@@ -359,6 +359,8 @@ class VerifierServletTransaction implements DocumentModeHandler, SchemaResolver 
 
     private Deque<Section> outline;
 
+    private Deque<Section> headingOutline;
+
     private boolean showSource;
 
     private boolean showOutline;
@@ -1051,12 +1053,15 @@ class VerifierServletTransaction implements DocumentModeHandler, SchemaResolver 
                 documentInput.setEncoding(charsetOverride);
             }
             if (showOutline) {
-                reader = new OutlineBuildingXMLReaderWrapper(reader, request);
+                reader = new OutlineBuildingXMLReaderWrapper(reader, request, false);
+                reader = new OutlineBuildingXMLReaderWrapper(reader, request, true);
             }
             reader.parse(documentInput);
             if (showOutline) {
                 outline = (Deque<Section>) request.getAttribute(
                         "http://validator.nu/properties/document-outline");
+                headingOutline = (Deque<Section>) request.getAttribute(
+                        "http://validator.nu/properties/heading-outline");
             }
         } catch (CannotRecoverException e) {
         } catch (ChangingEncodingException e) {
@@ -1103,7 +1108,7 @@ class VerifierServletTransaction implements DocumentModeHandler, SchemaResolver 
         }
         if (isHtmlOrXhtml) {
             XhtmlOutlineEmitter outlineEmitter = new XhtmlOutlineEmitter(
-                    contentHandler, outline);
+                    contentHandler, outline, headingOutline);
             outlineEmitter.emitHeadings();
             outlineEmitter.emit();
             emitDetails();

src/nu/validator/servlet/XhtmlOutlineEmitter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012 Vadim Zaslawski, Ontos AG
- * Copyright (c) 2012-2016 Mozilla Foundation
+ * Copyright (c) 2012-2017 Mozilla Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -35,20 +35,25 @@ import org.xml.sax.SAXException;
 
 public class XhtmlOutlineEmitter {
 
-    private static final char[] OUTLINE = "Structural outline".toCharArray();
+    private static final char[] OUTLINE = //
+            "Structural outline".toCharArray();
 
-    private static final char[] HEADINGOUTLINE = "Heading-level outline".toCharArray();
+    private static final char[] HEADINGOUTLINE = //
+            "Heading-level outline".toCharArray();
 
     private final Deque<Section> outline;
+
+    private final Deque<Section> headingOutline;
 
     private final XhtmlSaxEmitter emitter;
 
     private final AttributesImpl attrs = new AttributesImpl();
 
     public XhtmlOutlineEmitter(final ContentHandler contentHandler,
-            final Deque<Section> outline) {
+            final Deque<Section> outline, final Deque<Section> headingOutline) {
         this.emitter = new XhtmlSaxEmitter(contentHandler);
         this.outline = outline;
+        this.headingOutline = headingOutline;
     }
 
     public void emit() throws SAXException {
@@ -99,7 +104,7 @@ public class XhtmlOutlineEmitter {
         emittedDummyH3 = false;
         emittedDummyH4 = false;
         emittedDummyH5 = false;
-        if (outline != null) {
+        if (headingOutline != null) {
             attrs.clear();
             attrs.addAttribute("id", "headingoutline");
             emitter.startElement("section", attrs);
@@ -107,7 +112,7 @@ public class XhtmlOutlineEmitter {
             emitter.characters(HEADINGOUTLINE);
             emitter.endElement("h2");
             try {
-                emitHeadingOutline(outline, 0);
+                emitHeadingOutline(headingOutline, 0);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -162,9 +167,9 @@ public class XhtmlOutlineEmitter {
         emitter.endElement("ol");
     }
 
-    protected void emitHeadingOutline(Deque<Section> outline, int currentDepth)
-            throws IOException, SAXException {
-        for (Section section : outline) {
+    protected void emitHeadingOutline(Deque<Section> headingOutline,
+            int currentDepth) throws IOException, SAXException {
+        for (Section section : headingOutline) {
             String headingName = section.getHeadingElementName();
             if ("h1".equals(headingName)) {
                 hasH1 = true;