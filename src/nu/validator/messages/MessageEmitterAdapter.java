50f43822e9a2a3b227356b65f92f467e8db55d98
Michael[tm] Smith <mike@w3.org>
2018-08-21 15:35:14 +0900

    [css] Drop 'justify-content' errors
    This change is a hack to work around the fact the CSS-checking backend
    doesn’t yet have up-to-date support for the `justify-content` property.
    This change causes the checker to filter out (drop/ignore/suppress)
    any error messages about `justify-content` values.
    
    Addresses https://github.com/validator/validator/issues/690

--- src/nu/validator/messages/MessageEmitterAdapter.java
+++ src/nu/validator/messages/MessageEmitterAdapter.java
@@ -392,6 +392,7 @@ public class MessageEmitterAdapter implements ErrorHandler {
             ".*Property \u201Cfont-display\u201D doesn't exist.*", //
             ".*Property \u201Crow-gap\u201D doesn't exist.*", //
             ".*\u201Cstart\u201D is not a \u201Calign-content\u201D value.*", //
+            ".*\u201Cjustify-content\u201D.*", //
     };
 
     protected static final Pattern DEFAULT_FILTER_PATTERN = Pattern.compile(
