2a0fb2840e9f2b57977e14684648e856e8db0e1f
Takeshi Kurosawa <taken.spc@gmail.com>
2019-10-16 12:04:19 +0900

    Tweak warnings for hspace and vspace attrs
    HTML Standard lists iframe and input elements' hspace and vspace attrs on "Use CSS instead." of obsolete features while Nu Html Checker misses these elements.
    
    https://html.spec.whatwg.org/multipage/obsolete.html#non-conforming-features
    
    This commit adds iframe and input elements to the OBSOLETE_STYLE_ATTRS map and sort the element names to align with HTML Standard.

--- src/nu/validator/checker/schematronequiv/Assertions.java
+++ src/nu/validator/checker/schematronequiv/Assertions.java
@@ -326,7 +326,7 @@ public class Assertions extends Checker {
         OBSOLETE_STYLE_ATTRS.put("frame", new String[] { "table" });
         OBSOLETE_STYLE_ATTRS.put("height", new String[] { "td", "th" });
         OBSOLETE_STYLE_ATTRS.put("hspace",
-                new String[] { "img", "object", "embed" });
+                new String[] { "embed", "iframe", "input", "img", "object" });
         OBSOLETE_STYLE_ATTRS.put("link", new String[] { "body" });
         OBSOLETE_STYLE_ATTRS.put("bottommargin", new String[] { "body" });
         OBSOLETE_STYLE_ATTRS.put("marginheight",
@@ -347,7 +347,7 @@ public class Assertions extends Checker {
                 "tbody", "td", "tfoot", "th", "thead", "tr" });
         OBSOLETE_STYLE_ATTRS.put("vlink", new String[] { "body" });
         OBSOLETE_STYLE_ATTRS.put("vspace",
-                new String[] { "img", "object", "embed" });
+                new String[] { "embed", "iframe", "input", "img", "object" });
         OBSOLETE_STYLE_ATTRS.put("width", new String[] { "hr", "table", "td",
                 "th", "col", "colgroup", "pre" });
     }
