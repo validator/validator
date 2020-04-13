e4ca4eb7a8fcbf73ece917094a752091e5942204
Michael[tm] Smith <mike@w3.org>
2020-04-13 11:17:36 +0900

    Code clarity

--- src/nu/validator/datatype/AbstractAutocompleteDetails.java
+++ src/nu/validator/datatype/AbstractAutocompleteDetails.java
@@ -33,75 +33,75 @@ abstract class AbstractAutocompleteDetails extends AbstractDatatype {
         super();
     }
 
-    private static final HashSet<String> contactTypes = new HashSet<>();
+    private static final HashSet<String> CONTACT_TYPES = new HashSet<>();
 
-    private static final HashSet<String> allFieldnames = new HashSet<>();
+    private static final HashSet<String> ALL_FIELD_NAMES = new HashSet<>();
 
     private static final HashSet<String> allowedFieldnames = new HashSet<>();
 
     private static final HashSet<String> allowedContactFieldnames = new HashSet<>();
 
     static {
-        contactTypes.add("home");
-        contactTypes.add("work");
-        contactTypes.add("mobile");
-        contactTypes.add("fax");
-        contactTypes.add("pager");
+        CONTACT_TYPES.add("home");
+        CONTACT_TYPES.add("work");
+        CONTACT_TYPES.add("mobile");
+        CONTACT_TYPES.add("fax");
+        CONTACT_TYPES.add("pager");
 
-        allFieldnames.add("name");
-        allFieldnames.add("honorific-prefix");
-        allFieldnames.add("given-name");
-        allFieldnames.add("additional-name");
-        allFieldnames.add("family-name");
-        allFieldnames.add("honorific-suffix");
-        allFieldnames.add("nickname");
-        allFieldnames.add("organization-title");
-        allFieldnames.add("username");
-        allFieldnames.add("new-password");
-        allFieldnames.add("current-password");
-        allFieldnames.add("one-time-code");
-        allFieldnames.add("organization");
-        allFieldnames.add("street-address");
-        allFieldnames.add("address-line1");
-        allFieldnames.add("address-line2");
-        allFieldnames.add("address-line3");
-        allFieldnames.add("address-level4");
-        allFieldnames.add("address-level3");
-        allFieldnames.add("address-level2");
-        allFieldnames.add("address-level1");
-        allFieldnames.add("country");
-        allFieldnames.add("country-name");
-        allFieldnames.add("postal-code");
-        allFieldnames.add("cc-name");
-        allFieldnames.add("cc-given-name");
-        allFieldnames.add("cc-additional-name");
-        allFieldnames.add("cc-family-name");
-        allFieldnames.add("cc-number");
-        allFieldnames.add("cc-exp");
-        allFieldnames.add("cc-exp-month");
-        allFieldnames.add("cc-exp-year");
-        allFieldnames.add("cc-csc");
-        allFieldnames.add("cc-type");
-        allFieldnames.add("transaction-currency");
-        allFieldnames.add("transaction-amount");
-        allFieldnames.add("language");
-        allFieldnames.add("bday");
-        allFieldnames.add("bday-day");
-        allFieldnames.add("bday-month");
-        allFieldnames.add("bday-year");
-        allFieldnames.add("sex");
-        allFieldnames.add("url");
-        allFieldnames.add("photo");
-        allFieldnames.add("tel");
-        allFieldnames.add("tel-country-code");
-        allFieldnames.add("tel-national");
-        allFieldnames.add("tel-area-code");
-        allFieldnames.add("tel-local");
-        allFieldnames.add("tel-local-prefix");
-        allFieldnames.add("tel-local-suffix");
-        allFieldnames.add("tel-extension");
-        allFieldnames.add("email");
-        allFieldnames.add("impp");
+        ALL_FIELD_NAMES.add("name");
+        ALL_FIELD_NAMES.add("honorific-prefix");
+        ALL_FIELD_NAMES.add("given-name");
+        ALL_FIELD_NAMES.add("additional-name");
+        ALL_FIELD_NAMES.add("family-name");
+        ALL_FIELD_NAMES.add("honorific-suffix");
+        ALL_FIELD_NAMES.add("nickname");
+        ALL_FIELD_NAMES.add("organization-title");
+        ALL_FIELD_NAMES.add("username");
+        ALL_FIELD_NAMES.add("new-password");
+        ALL_FIELD_NAMES.add("current-password");
+        ALL_FIELD_NAMES.add("one-time-code");
+        ALL_FIELD_NAMES.add("organization");
+        ALL_FIELD_NAMES.add("street-address");
+        ALL_FIELD_NAMES.add("address-line1");
+        ALL_FIELD_NAMES.add("address-line2");
+        ALL_FIELD_NAMES.add("address-line3");
+        ALL_FIELD_NAMES.add("address-level4");
+        ALL_FIELD_NAMES.add("address-level3");
+        ALL_FIELD_NAMES.add("address-level2");
+        ALL_FIELD_NAMES.add("address-level1");
+        ALL_FIELD_NAMES.add("country");
+        ALL_FIELD_NAMES.add("country-name");
+        ALL_FIELD_NAMES.add("postal-code");
+        ALL_FIELD_NAMES.add("cc-name");
+        ALL_FIELD_NAMES.add("cc-given-name");
+        ALL_FIELD_NAMES.add("cc-additional-name");
+        ALL_FIELD_NAMES.add("cc-family-name");
+        ALL_FIELD_NAMES.add("cc-number");
+        ALL_FIELD_NAMES.add("cc-exp");
+        ALL_FIELD_NAMES.add("cc-exp-month");
+        ALL_FIELD_NAMES.add("cc-exp-year");
+        ALL_FIELD_NAMES.add("cc-csc");
+        ALL_FIELD_NAMES.add("cc-type");
+        ALL_FIELD_NAMES.add("transaction-currency");
+        ALL_FIELD_NAMES.add("transaction-amount");
+        ALL_FIELD_NAMES.add("language");
+        ALL_FIELD_NAMES.add("bday");
+        ALL_FIELD_NAMES.add("bday-day");
+        ALL_FIELD_NAMES.add("bday-month");
+        ALL_FIELD_NAMES.add("bday-year");
+        ALL_FIELD_NAMES.add("sex");
+        ALL_FIELD_NAMES.add("url");
+        ALL_FIELD_NAMES.add("photo");
+        ALL_FIELD_NAMES.add("tel");
+        ALL_FIELD_NAMES.add("tel-country-code");
+        ALL_FIELD_NAMES.add("tel-national");
+        ALL_FIELD_NAMES.add("tel-area-code");
+        ALL_FIELD_NAMES.add("tel-local");
+        ALL_FIELD_NAMES.add("tel-local-prefix");
+        ALL_FIELD_NAMES.add("tel-local-suffix");
+        ALL_FIELD_NAMES.add("tel-extension");
+        ALL_FIELD_NAMES.add("email");
+        ALL_FIELD_NAMES.add("impp");
     }
 
     @Override
@@ -137,7 +137,7 @@ abstract class AbstractAutocompleteDetails extends AbstractDatatype {
         if (detailTokens.size() < 1) {
             return;
         }
-        if (contactTypes.contains(detailTokens.get(0))) {
+        if (CONTACT_TYPES.contains(detailTokens.get(0))) {
             isContactDetails = true;
             contactType = detailTokens.get(0);
             detailTokens.remove(0);
@@ -169,13 +169,13 @@ abstract class AbstractAutocompleteDetails extends AbstractDatatype {
             detailTokens.remove(0);
         }
         if (detailTokens.size() > 0
-                && contactTypes.contains(detailTokens.get(0))) {
+                && CONTACT_TYPES.contains(detailTokens.get(0))) {
             isContactDetails = true;
             contactType = detailTokens.get(0);
             detailTokens.remove(0);
         }
         for (String token : detailTokens) {
-            if (contactTypes.contains(token)) {
+            if (CONTACT_TYPES.contains(token)) {
                 throw newDatatypeException(
                         "The token \u201c" + token + "\u201d must only"
                                 + " appear before any autofill field names.");
@@ -192,7 +192,7 @@ abstract class AbstractAutocompleteDetails extends AbstractDatatype {
                                 + " token is a \u201csection-*\u201d indicator,"
                                 + " as the second token.");
             }
-            if (!allFieldnames.contains(token)) {
+            if (!ALL_FIELD_NAMES.contains(token)) {
                 throw newDatatypeException("The string \u201c" + token
                         + "\u201d is not a valid autofill field name.");
             }
