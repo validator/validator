0b423accb6fb126aa16078ace68048e25ad58b66
Michael[tm] Smith <mike@w3.org>
2020-10-06 13:28:47 +0900

    Revert "[css] Drop 'color-scheme' errors"
    This reverts commit f16d3a138e9577413de8b4deab90c8c39d5533f5.

--- src/nu/validator/messages/MessageEmitterAdapter.java
+++ src/nu/validator/messages/MessageEmitterAdapter.java
@@ -390,7 +390,6 @@ public class MessageEmitterAdapter implements ErrorHandler {
             // (non-)errors for features the CSS checker doesn’t support yet
             ".*Unknown pseudo-element or pseudo-class \u201C:focus-within\u201D.*", //
             ".*leader(.+)is not a \u201Ccontent\u201D value.*", //
-            ".*color-scheme.*", //
     };
 
     protected static final Pattern DEFAULT_FILTER_PATTERN = Pattern.compile(
