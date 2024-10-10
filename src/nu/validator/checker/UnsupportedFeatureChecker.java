5eb83c83a9499b6aa4cce2f6b959c798c9c02003
sideshowbarker <mike@w3.org>
2024-10-10 14:34:05 +0900

    Drop UnsupportedFeatureChecker::warnAboutElement

--- src/nu/validator/checker/UnsupportedFeatureChecker.java
+++ src/nu/validator/checker/UnsupportedFeatureChecker.java
@@ -38,12 +38,6 @@ public class UnsupportedFeatureChecker extends Checker {
                 + " using a polyfill.");
     }
 
-    private void warnAboutElement(String name) throws SAXException {
-        warn("The \u201C" + name + "\u201D element is not supported in"
-                + " all browsers. Please be sure to test, and consider"
-                + " using a polyfill.");
-    }
-
     private void warnAboutAttributeOnElement(String attributeName, String elementName) throws SAXException {
       warn("The \u201C" + attributeName + "\u201D attribute on the"
           + " \u201C" + elementName + "\u201D element is not supported"
