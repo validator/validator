d1a9513bb5ac8c93e9b20199254ad55aecde507d
Viktor Szépe <viktor@szepe.net>
2024-10-31 10:26:38 +0000

    Revert regexp class

--- src/nu/validator/servlet/VerifierServletTransaction.java
+++ src/nu/validator/servlet/VerifierServletTransaction.java
@@ -154,7 +154,7 @@ class VerifierServletTransaction implements DocumentModeHandler, SchemaResolver 
 
     private static final Pattern SPACE = Pattern.compile("\\s+");
 
-    private static final Pattern JS_IDENTIFIER = Pattern.compile("[\\p{Lu}\\p{Ll}\\p{Lt}\\p{Lm}\\p{Lo}\\p{Nl}_\\$][\\p{Lu}\\p{Ll}\\p{Lt}\\p{Lm}\\p{Lo}\\p{Nl}_\\$\\p{Mn}\\p{Mc}\\p{And}\\p{Pc}]*");
+    private static final Pattern JS_IDENTIFIER = Pattern.compile("[\\p{Lu}\\p{Ll}\\p{Lt}\\p{Lm}\\p{Lo}\\p{Nl}_\\$][\\p{Lu}\\p{Ll}\\p{Lt}\\p{Lm}\\p{Lo}\\p{Nl}_\\$\\p{Mn}\\p{Mc}\\p{Nd}\\p{Pc}]*");
 
     private static final String[] JS_RESERVED_WORDS = { "abstract", "boolean",
             "break", "byte", "case", "catch", "char", "class", "const",
