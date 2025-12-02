validator/validator

commit 80ec8a5220221b8f4a17e2ae36d7a2dc71ae1f3e

feat: “summary”: Disallow “role” and most non-global ARIA attributes
Fixes https://github.com/validator/validator/issues/1541

src/nu/validator/checker/schematronequiv/Assertions.java
@@ -457,6 +457,14 @@ public class Assertions extends Checker {
             "noscript", "param", "script", "slot", "source", "style",
             "template", "title", "track" };
 
+    private static final String[] ARIA_GLOBAL_ATTRIBUTES = { "aria-atomic",
+        "aria-braillelabel", "aria-brailleroledescription", "aria-busy",
+        "aria-controls", "aria-current", "aria-describedby", "aria-details",
+        "aria-disabled", "aria-errormessage", "aria-flowto", "aria-haspopup",
+        "aria-hidden", "aria-invalid", "aria-keyshortcuts", "aria-label",
+        "aria-labelledby", "aria-live", "aria-owns", "aria-relevant",
+        "aria-roledescription", "aria-description" };
+
     private static final String[] INTERACTIVE_ELEMENTS = { "a", "button",
             "details", "dialog", "embed", "iframe", "label", "select",
             "textarea" };
@@ -3970,6 +3978,9 @@ public class Assertions extends Checker {
             if ("figure" == localName) {
                 child.setIsCollectingChildren(true);
             }
+            if ("details" == localName) {
+                child.setIsCollectingChildren(true);
+            }
             if (activeDescendant != null && !activeDescendantWithAriaOwns) {
                 openActiveDescendants.put(child,
                         new LocatorImpl(getDocumentLocator()));
@@ -4067,6 +4078,23 @@ public class Assertions extends Checker {
                     }
                 } else {
                         // ARIA
+                }
+            }
+            if ("summary" == localName && "details".equals(parent.name)
+                    && !parent.getCollectedChildren().stream().anyMatch(
+                        s -> "summary" == s.getName())) {
+                for (int i = 0; i < atts.getLength(); i++) {
+                    String attLocal = atts.getLocalName(i);
+                    if ("role".equals(attLocal)
+                            || (Arrays.binarySearch(ARIA_GLOBAL_ATTRIBUTES,
+                                    attLocal) < 0
+                                && !"aria-haspopup".equals(attLocal)
+                                && !"aria-disabled".equals(attLocal))) {
+                        err("The \u201C" + attLocal + "\u201D attribute must"
+                                + " not be used on any \u201Csummary\u201D"
+                                + " element that is a summary for its parent"
+                                + " \u201Cdetails\u201D element.");
+                    }
                 }
             }
             if (parent != null && parent.isCollectingChildren()) {

schema/html5/applications.rnc
@@ -248,9 +248,7 @@ datatypes w = "http://whattf.org/datatype-draft"
 		element summary { summary.inner & summary.attrs }
 	summary.attrs =
 		(	common.attrs
-		&	(	common.attrs.aria.implicit.button
-			|	common.attrs.aria.role.button
-			)?
+		&	common.attrs.aria?
 		)
 	summary.inner =
 		(	common.inner.phrasing

tests/html-aria/misc/summary-for-its-details-with-aria-disabled-isvalid.html
@@ -1,0 +1,5 @@
+<!doctype html><html lang><meta charset=utf-8>
+<title>test</title>
+<details open>
+  <summary aria-disabled=true>test 1</summary>
+</details>

tests/html-aria/misc/summary-for-its-details-with-aria-expanded-novalid.html
@@ -1,0 +1,5 @@
+<!doctype html><html lang><meta charset=utf-8>
+<title>test</title>
+<details open>
+  <summary aria-expanded=false>test 1</summary>
+</details>

tests/html-aria/misc/summary-for-its-details-with-aria-haspopup-isvalid.html
@@ -1,0 +1,5 @@
+<!doctype html><html lang><meta charset=utf-8>
+<title>test</title>
+<details open>
+  <summary aria-haspopup=true>test 2</summary>
+</details>

tests/html-aria/misc/summary-for-its-details-with-aria-pressed-novalid.html
@@ -1,0 +1,5 @@
+<!doctype html><html lang><meta charset=utf-8>
+<title>test</title>
+<details open>
+  <summary aria-pressed=true>test 2</summary>
+</details>

tests/html-aria/misc/summary-for-its-details-with-role-paragraph-novalid.html
@@ -1,0 +1,5 @@
+<!doctype html><html lang><meta charset=utf-8>
+<title>test</title>
+<details open>
+  <summary role=paragraph>test</summary>
+</details>

commit 501390ea67a313aa1eb4c6460934f25f0f98a5f1

fix: Un-bork regression caused by 80ec8a5220 change
See https://github.com/MaxMood96/validator/commit/80ec8a5220221b8f4a17e2ae36d7a2dc71ae1f3e

src/nu/validator/checker/schematronequiv/Assertions.java
@@ -4094,7 +4094,8 @@ public class Assertions extends Checker {
                 for (int i = 0; i < atts.getLength(); i++) {
                     String attLocal = atts.getLocalName(i);
                     if ("role".equals(attLocal)
-                            || (Arrays.binarySearch(ARIA_GLOBAL_ATTRIBUTES,
+                            || (attLocal.startsWith("aria-")
+                                && Arrays.binarySearch(ARIA_GLOBAL_ATTRIBUTES,
                                     attLocal) < 0
                                 && !"aria-haspopup".equals(attLocal)
                                 && !"aria-disabled".equals(attLocal))) {

tests/html/elements/summary/summary-with-attributes-isvalid.html
@@ -1,0 +1,16 @@
+<!doctype html><html lang><meta charset=utf-8>
+<title>test</title>
+ <details name="frame-characteristics">
+  <summary class=foo>Material</summary>
+  The picture frame is made of solid oak wood.
+ </details>
+ <details name="frame-characteristics">
+  <summary lang=ja>大きさ</summary>
+  The picture frame fits a photo 40cm tall and 30cm wide.
+  The frame is 45cm tall, 35cm wide, and 2cm thick.
+ </details>
+ <details name="frame-characteristics">
+  <summary title=bar>Color</summary>
+  The picture frame is available in its natural wood
+  color, or with black stain.
+ </details>