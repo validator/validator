f6fbca4edd1a8d5fa726b3565ea67ae321ed39b3
tenzap <fabstz-it@yahoo.fr>
2026-05-01 15:55:54 +0200

    Upgrade to jetty 12.0

--- src/nu/validator/servlet/Main.java
+++ src/nu/validator/servlet/Main.java
@@ -51,9 +51,9 @@ import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.gzip.GzipHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.FilterHolder;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 /**
