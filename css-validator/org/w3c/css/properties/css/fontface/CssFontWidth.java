bc0acbe4aabfcf26e4140a8c6987eb4d834483cd
Levent Dag <levent@handig-eekhoorn.at>
2026-07-22 16:36:33 +0900

    fix(css-validator): Correct the font-width property name casing
    Fix the property name from "font-Width" to "font-width".

--- css-validator/org/w3c/css/properties/css/fontface/CssFontWidth.java
+++ css-validator/org/w3c/css/properties/css/fontface/CssFontWidth.java
@@ -53,7 +53,7 @@ public class CssFontWidth extends CssProperty {
      * Returns the name of this property
      */
     public final String getPropertyName() {
-        return "font-Width";
+        return "font-width";
     }
 
     /**
